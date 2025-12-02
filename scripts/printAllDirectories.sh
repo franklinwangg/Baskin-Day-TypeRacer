c62bfcb5a86142447226b6f8cf1af6878a3231d6
Franklin Wang <franklinywang@gmail.com>
2025-12-01 16:26:14 -0800

    stopped tracking some stuff

---
+++ scripts/printAllDirectories.sh
@@ -0,0 +1,6 @@
+# find . \
+#   -type d \( -name "node_modules" -o -name ".git" -o -name "venv" -o -name "chrome-data" -o -name "jobs" \) -prune -o \
+#   -type f -print | sed 's|^\./||' > all_files.txt 
+find .. \
+  -type d \( -name "node_modules" -o -name ".git" -o -name "venv" -o -name "chrome-data" -o -name "jobs" \) -prune -o \
+  -type f -print > ../all_files.txt
